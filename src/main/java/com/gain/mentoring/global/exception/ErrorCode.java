950049042a23c81c170d7f86ed1cb24294cfc9e6
JoonKyoLee <joonkyolee@gmail.com>
2025-05-05 22:15:59 +0900

    feat: 회원가입에서 발생 가능한 오류 정리 - 발생 가능한 오류(존재하는 회원, 존재하는 아이디) 정리 및 HttpStatus와 에러 메시지 추가

--- src/main/java/com/gain/mentoring/global/exception/ErrorCode.java
+++ src/main/java/com/gain/mentoring/global/exception/ErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    DUPLICATED_USER("이미 존재하는 회원입니다.", HttpStatus.CONFLICT);
+    DUPLICATED_USER("이미 존재하는 회원입니다.", HttpStatus.CONFLICT),
+    DUPLICATED_USERID("이미 존재하는 아이디입니다.", HttpStatus.CONFLICT);
     // 추후 추가
 
     private final String message;
