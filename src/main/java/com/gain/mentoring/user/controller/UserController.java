9c5f9b1a8e7a802ea8f2e70b9568181ce0fd4537
JoonKyoLee <joonkyolee@gmail.com>
2025-05-05 23:18:18 +0900

    feat: 회원가입 API 엔드포인트 및 Swagger 응답 명세 추가 - POST /api/users/signup 엔드포인트 추가 - @Valid를 활용한 요청값 검증 적용 - 400, 409 에러에 대한 Swagger 명세서 작성 - 회원가입 성공 시 201 Created 응답 반환

---
+++ src/main/java/com/gain/mentoring/user/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.gain.mentoring.user.controller;
+
+import com.gain.mentoring.global.exception.ErrorResponse;
+import com.gain.mentoring.user.dto.request.SignupRequest;
+import com.gain.mentoring.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "회원", description = "회원 API")
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @Operation(summary = "회원가입")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "회원가입 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 입력 값이 전달된 경우",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "409", description = "이메일 또는 아이디 중복",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @PostMapping("/signup")
+    public ResponseEntity<Long> signup(@Valid @RequestBody SignupRequest request) {
+        Long id = userService.signup(request);
+        return ResponseEntity.created(URI.create("/api/users/" + id)).build();
+    }
+}
