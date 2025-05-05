51dbc9e3629890b314b9ee5eb8edd7b9f8a22f63
JoonKyoLee <joonkyolee@gmail.com>
2025-05-05 21:58:41 +0900

    feat: 이메일 및 존재 여부를 확인하기 위한 boolean 메서드 추가 - 중복 이메일 확인을 위한 existsByEmail 메서드 추가 - 중복 아이디 확인을 위한 existsByUserName 메서드 추가

--- src/main/java/com/gain/mentoring/user/repository/UserRepository.java
+++ src/main/java/com/gain/mentoring/user/repository/UserRepository.java
@@ -10,4 +10,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     // 아이디로 회원 찾기(이메일 회원가입)
     Optional<User> findByUserName(String userName);
+
+    // 중복 방지를 위한 존재 여부 확인
+    boolean existsByEmail(String email);
+    boolean existsByUserName(String userName);
 }
