c1f7cbaaf46fd94c6c62463809a72c3e932e2f4f
JoonKyoLee <joonkyolee@gmail.com>
2025-05-08 20:33:59 +0900

    feat: 이메일 로그인 요청 DTO 작성 - 로그인을 위한 LoginRequest 클래스 생성 - 이메일, 비밀번호 필드 포함 및 각 필드에 유효성 검사 적용 - Swagger 문서화를 위한 어노테이션 추가

---
+++ src/main/java/com/gain/mentoring/user/dto/request/LoginRequest.java
@@ -0,0 +1,30 @@
+package com.gain.mentoring.user.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+    @Schema(description = "이메일", example = "test1234@google.com")
+    @Pattern(
+            regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$",
+            message = "올바른 이메일 형식이 아닙니다."
+    )
+    @NotBlank(message = "이메일은 필수입니다.")
+    private String email;
+
+    @Schema(description = "비밀번호", example = "Test1234!")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+=-])[A-Za-z\\d!@#$%^&*()_+=-]{8,16}$",
+            message = "비밀번호는 8~16자의 영문, 숫자, 특수문자를 포함해야 합니다."
+    )
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    private String password;
+}
